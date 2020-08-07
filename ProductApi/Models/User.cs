1649629b4a42261b5b9c46ddbedae4c875cc050b
Rodrigo Losik <rodrigolosik@gmail.com>
2020-08-06 21:26:11 -0300

    User crud included, plus JWT generation.

---
+++ ProductApi/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProductApi.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string Password { get; set; }
+        public string Role { get; set; }
+    }
+}
---
+++ ProductApi/Repository/Implementation/UserRepository.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductApi.Data;
+using ProductApi.Models;
+using ProductApi.Services;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductApi.Repository.Implementation
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ApplicationContext _context;
+
+        public UserRepository(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(User user)
+        {
+            _context.Add(user);
+        }
+
+        public void Delete(User user)
+        {
+            _context.Remove(user);
+        }
+
+        public async Task<User> GetUser(string username, string password)
+        {
+            var users = await ListUsers();
+
+            return users.Where(x => 
+                x.UserName == username && 
+                UserService.GeneratePassword(x.Password) == UserService.GeneratePassword(password))
+            .FirstOrDefault();
+        }
+
+        public async Task<User> GetUser(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<User>> ListUsers()
+        {
+            return await _context.Users.AsNoTracking().ToListAsync();
+        }
+
+        public async Task<bool> SaveChangesAsync()
+        {
+            return (await _context.SaveChangesAsync() > 0);
+        }
+
+        public void Update(User user)
+        {
+            _context.Update(user);
+        }
+    }
+}
